368316c618f108fe0bd952115524619f35c1c100
master <dsaqaz421@naver.com>
2021-10-07 00:31:08 +0000

    fix graphql in production mode

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import {protectResolver} from "./shared/shared.utils.js";
 const server = new ApolloServer({ 	
 	typeDefs,
 	resolvers,
+	playground : true,		// production 모드에서도 /graphql 을 실행할 수 있도록 처리(확인 용도로만 사용하고 주석 처리하기)
+	introspection : true,	// production 모드에서도 query 실행 가능하도록 해줌
 	context : async(ctx) => {
 		return {
 			loggedInUser : await getUser(ctx.req.headers.token),
