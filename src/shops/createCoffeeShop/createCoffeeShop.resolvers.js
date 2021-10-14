a5f35ea5674ea932ad5a53b520473606b3e022f3
master <dsaqaz421@naver.com>
2021-10-14 23:36:40 +0000

    fixed

--- src/shops/createCoffeeShop/createCoffeeShop.resolvers.js
+++ src/shops/createCoffeeShop/createCoffeeShop.resolvers.js
@@ -5,42 +5,44 @@ import { uploadToS3 } from '../../shared/shared.utils';
 
 export default {
 	Mutation : {
-		createCoffeeShop : protectResolver(async(_, {name, caption, latitude, longitude, file}, {loggedInUser}) => {		
-			// category 추출하기
-			let categoriesObjs = [];
-			if(caption){
-				categoriesObjs = processCategories(caption);
-			}			
+		createCoffeeShop : protectResolver(async(_, {name, caption, latitude, longitude, file}, {loggedInUser}) => {	
+			try{
+				// category 추출하기
+				let categoriesObjs = [];
+				if(caption){
+					categoriesObjs = processCategories(caption);
+				}			
 
-			// file Url 가져옴
-			console.log("엥 이거 완전?");
-			const fileUrl = await uploadToS3(file, loggedInUser.id, 'uploads');
-			console.log(fileUrl);
+				// file Url 가져옴			
+				const fileUrl = await uploadToS3(file, loggedInUser.id, 'uploads');			
 
-			// shop 데이터 저장, photo 저장, category는 있으면 저장
-			return client.coffeeShop.create({
-				data : {
-					name,
-					caption,
-					user : {
-						connect : {
-							id : loggedInUser.id,
-						}
-					},
-					latitude,
-					longitude,
-					...(categoriesObjs.length > 0 && {
-						categories : {
-							connectOrCreate : categoriesObjs
-						}	
-					}),
-					photos : {
-						create : {
-							url : fileUrl
+				// shop 데이터 저장, photo 저장, category는 있으면 저장
+				return client.coffeeShop.create({
+					data : {
+						name,
+						caption,
+						user : {
+							connect : {
+								id : loggedInUser.id,
+							}
+						},
+						latitude,
+						longitude,
+						...(categoriesObjs.length > 0 && {
+							categories : {
+								connectOrCreate : categoriesObjs
+							}	
+						}),
+						photos : {
+							create : {
+								url : fileUrl
+							}
 						}
 					}
-				}
-			});
+				});	
+			}catch(e){
+				return e;
+			}			
 		}),
 	}
 }
