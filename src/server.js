20a29d9bed7738cb4229eef4b5a59565843af22b
master <dsaqaz421@naver.com>
2021-10-14 23:35:08 +0000

    fix playground none

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ const cors = require("cors");
 const apollo = new ApolloServer({ 		
 	resolvers,
 	typeDefs,
-	playground : true,		// production 모드에서도 /graphql 을 실행할 수 있도록 처리(확인 용도로만 사용하고 주석 처리하기)
-	introspection : true,	// production 모드에서도 query 실행 가능하도록 해줌
+	//playground : true,		// production 모드에서도 /graphql 을 실행할 수 있도록 처리(확인 용도로만 사용하고 주석 처리하기)
+	//introspection : true,	// production 모드에서도 query 실행 가능하도록 해줌
 	context : async(ctx) => {
 		return {
 			loggedInUser : await getUser(ctx.req.headers.token),
